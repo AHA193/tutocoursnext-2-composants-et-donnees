1b42e7909bbd8bc66ef46ac3f2421db510f47028
Aziz <azizhoumani@gmail.com>
2025-07-16 18:15:21 +0200

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="p-20">
+        {children}
+      </body>
+    </html>
+  );
+}
