9366e86e8da1e8f19ab51be58e4f8632aacab6aa
Aziz <azizhoumani@gmail.com>
2025-07-16 22:24:52 +0200

    changement de la var res mais ss affichage de l_image

--- src/app/image/page.jsx
+++ src/app/image/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 async function page() {
 
 
-const res = await fetch ("https://jsonplaceholder.typicode.com/photos/1") /**/
+const res = await fetch ("https://jsonplaceholder.typicode.com/albums/1/photos/1") /*https://jsonplaceholder.typicode.com/photos/1*/
 const imgObject = await res.json()
 console.log(imgObject);
 
@@ -13,8 +13,7 @@ console.log(imgObject);
         <div>
         <h1>Une image</h1>
         <img src={imgObject.url} 
-          placeholder={imgObject.url} 
-    alt="" />
+        alt="" />
         </div>
   )
 }
